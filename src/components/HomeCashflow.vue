58e146487274410a517695adf9971cecb60b5fe6
Jose Flores <charlijflo@gmail.com>
2022-03-26 17:21:48 -0600

    Set Up Inicial

---
+++ src/components/HomeCashflow.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <template #header>
+      <Header />
+    </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from "./LayoutCashflow.vue";
+import Header from "./HeaderCashflow.vue";
+import Resume from "./Resume/IndexCashflow.vue";
+import Movements from "./MovementsCashflow.vue";
+export default {
+  components: {
+    Layout,
+    Header,
+    Resume,
+    Movements,
+  },
+};
+</script>
